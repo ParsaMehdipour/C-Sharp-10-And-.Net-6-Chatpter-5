1501f0136a0fb26e16d2cb317139aa5493ca849b
Parsa <Mehdipourparsa@gmail.com>
2022-06-19 18:40:29 +0430

    Add project files.

---
+++ Chatpter 5_Console/Program.cs
@@ -0,0 +1,30 @@
+﻿using Funtionalities;
+using static System.Console;
+
+Person bob = new()
+{
+    Name = "Bob",
+    DateOfBirth = DateTime.Today.AddYears(-29),
+};
+
+Person alice = new()
+{
+    Name = "Alice",
+    DateOfBirth = DateTime.Today.AddYears(-24),
+};
+
+//Person.Procreate(bob, alice, "janice");
+
+bob.ProcreateWith(alice, "janice").WriteToConsole();
+
+Person SecondBaby = Person.Procreate(bob, alice, "Mark");
+
+Person ThirdBaby = bob * alice;
+
+//bob.WriteToConsole();
+//bob.Children.First().WriteToConsole();
+
+//alice.WriteToConsole();
+//alice.Children.First().WriteToConsole();
+
+ReadLine();
---
+++ Funtionalities/Person.cs
@@ -0,0 +1,45 @@
+﻿using static System.Console;
+
+namespace Funtionalities;
+public class Person
+{
+    // fields
+    public string? Name; // ? allows null
+    public DateTime DateOfBirth;
+    public List<Person> Children = new(); // C# 9 or later
+
+    // methods
+    public void WriteToConsole()
+    {
+        WriteLine($"{Name} was born on a {DateOfBirth:dddd}.");
+    }
+
+    // static method to "multiply"
+    public static Person Procreate(Person p1, Person p2, string name)
+    {
+        Person baby = new()
+        {
+            Name = $"Baby of {p1.Name} and {p2.Name} who's name is {name}"
+        };
+        p1.Children.Add(baby);
+        p2.Children.Add(baby);
+        return baby;
+    }
+
+    public static Person operator *(Person p1, Person p2)
+    {
+        return Procreate(p1, p2, "Baby");
+    }
+
+    // instance method to "multiply"
+    public Person ProcreateWith(Person partner, string name)
+    {
+        return Procreate(this, partner, name);
+    }
+
+    public int MethodIWantToCall(string input)
+    {
+        return input.Length; // it doesn't matter what the method does
+    }
+
+}
