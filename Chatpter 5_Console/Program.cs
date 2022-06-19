a9dac563b3096d17a9fa7e6c629d5b1c960ec102
Parsa <Mehdipourparsa@gmail.com>
2022-06-20 00:37:05 +0430

    generic and non-generic types

--- Chatpter 5_Console/Program.cs
+++ Chatpter 5_Console/Program.cs
@@ -27,4 +27,34 @@ Person ThirdBaby = bob * alice;
 //alice.WriteToConsole();
 //alice.Children.First().WriteToConsole();
 
+// non-generic lookup collection
+System.Collections.Hashtable lookupObject = new();
+lookupObject.Add(key: 1, value: "Alpha"); //Key Is An Integer
+lookupObject.Add(key: 2, value: "Beta");
+lookupObject.Add(key: 3, value: "Gamma");
+lookupObject.Add(key: "harry", value: "Delta"); //Key Is A String
+
+int key = 2; // lookup the value that has 2 as its key
+WriteLine(format: "Key {0} has value: {1}",
+arg0: key,
+arg1: lookupObject[key]);
+
+// lookup the value that has harry as its key
+WriteLine(format: "Key {0} has value: {1}",
+arg0: "harry",
+arg1: lookupObject["harry"]);
+
+
+// generic lookup collection
+Dictionary<int, string> lookupIntString = new();
+lookupIntString.Add(key: 1, value: "Alpha");
+lookupIntString.Add(key: 2, value: "Beta");
+lookupIntString.Add(key: 3, value: "Gamma");
+lookupIntString.Add(key: 4, value: "Delta");
+
+key = 3;
+WriteLine(format: "Key {0} has value: {1}",
+arg0: key,
+arg1: lookupIntString[key]);
+
 ReadLine();
